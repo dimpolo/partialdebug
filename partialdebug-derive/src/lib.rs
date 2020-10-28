37fce8f5c653c791d36a9e02766e5b27d3a6a900
Dimitri Polonski <dimi.polonski@gmail.com>
2020-10-28 16:20:50 +0100

    quick and dirty type_info PartialDebug

--- partialdebug-derive/src/lib.rs
+++ partialdebug-derive/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::*;
 
 #[proc_macro_derive(NonExhaustivePartialDebug)]
@@ -42,6 +42,49 @@ pub fn derive_non_exhaustive(input: TokenStream) -> TokenStream {
                 } else {
                     s.finish_non_exhaustive()
                 }
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+#[proc_macro_derive(TypeInfoPartialDebug)]
+pub fn derive_type_info(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as ItemStruct);
+    let name = input.ident;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+
+    let fields = match input.fields {
+        Fields::Named(FieldsNamed { named, .. }) => named,
+        Fields::Unnamed(_) => unimplemented!(),
+        Fields::Unit => unimplemented!(),
+    };
+
+    let as_debug_all_fields = fields.iter().map(|field| {
+        let name = &field.ident;
+        let mut type_name = field.ty.to_token_stream().to_string();
+        type_name.retain(|c| !c.is_whitespace()); // remove whitespace
+
+        quote! {
+            .field(
+                stringify!(#name),
+                match ::partialdebug::AsDebug::as_debug(&self.#name){
+                    None => &::partialdebug::Placeholder(#type_name),
+                    Some(field) => field,
+                },
+            )
+        }
+    });
+
+    let expanded = quote! {
+        impl #impl_generics ::core::fmt::Debug for #name #ty_generics #where_clause{
+            fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+                f.debug_struct(stringify!(#name))
+
+                #(#as_debug_all_fields)*
+
+                .finish()
             }
         }
     };
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(incomplete_features)]
 #![feature(specialization)]
 
-use core::fmt::Debug;
+use core::fmt::{Debug, Formatter, Result};
 
 pub trait AsDebug {
     fn as_debug(&self) -> Option<&dyn Debug>;
@@ -19,6 +19,18 @@ impl<T: Debug> AsDebug for T {
     }
 }
 
+pub struct Placeholder(pub &'static str);
+
+impl Debug for Placeholder {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        f.debug_tuple(self.0).finish()
+    }
+}
+
 pub mod non_exhaustive {
     pub use partialdebug_derive::NonExhaustivePartialDebug as PartialDebug;
+}
+
+pub mod type_info {
+    pub use partialdebug_derive::TypeInfoPartialDebug as PartialDebug;
 }
